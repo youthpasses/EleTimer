71eccb920d3b41c8389d404d2a6c73461dc69efe
youthpasses <imakai@163.com>
2013-01-10 14:43:04 +0800

    Initial Commit

---
+++ EleTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EleTimer
+//
+//  Created by makai on 13-1-10.
+//  Copyright (c) 2013年 makai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EleTimer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EleTimer
+//
+//  Created by makai on 13-1-10.
+//  Copyright (c) 2013年 makai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
